da4be45fc5e7592ba792074389dfb5a5b707f9d4
chalasbh <chalasbh@OA-S141-08>
2025-03-19 11:59:48 -0400

    coding gyn

---
+++ Assets/Scripts/carrot.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class carrot : MonoBehaviour
+{
+    public float t;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+        }
+    }
+    public void CarLerper()
+    {
+        Vector2.Lerp
+    }
+}
