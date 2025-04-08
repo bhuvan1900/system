4a235e07e95495892f3882da1419cc9d149e0e1a
chalasbh <chalasbh@OA-S141-08>
2025-04-07 22:43:46 -0400

    found a more elegant way to do collisions

--- Assets/Scripts/ultramarines.cs
+++ Assets/Scripts/ultramarines.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class ultramarines : MonoBehaviour
 {
     public float umspeed = 1f;
-    public float umcircle = 1f;
+    public float umcircle = 0.2f;
+    public float ahrihorizontalcircle = 0.2f;
     public ahriman ahri;
-   
+    private Transform ahrimansform;
+    public float ysize = 0.2f;
+
+    private void Start()
+    {
+        if (ahri != null)
+        {
+            ahrimansform = ahri.transform;
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,13 +37,14 @@ public class ultramarines : MonoBehaviour
 
     void collisioncheck()
     {
-        float distance = Vector2.Distance(transform.position, ahri.transform.position);
-        //took code from here https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Distance.html
+        float distancedistance = transform.position.x - ahrimansform.position.x;
+        float distancedistance2 = transform.position.y - ahrimansform.position.y;
 
-        if (distance < umcircle + ahri.ahricircle)
+        if (distancedistance2 < ysize && distancedistance2 > -ysize && distancedistance < umcircle + ahrihorizontalcircle && distancedistance > -(umcircle + ahrihorizontalcircle))
         {
-            collisionresult();
+           collisionresult();
         }
+
     }
     void collisionresult()
     {
